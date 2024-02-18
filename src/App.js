9f85d6776a6a143e5ae7f239314e6904fe25b84f
MVinicius03 <156372749+MVinicius03@users.noreply.github.com>
2024-02-18 17:56:10 -0300

    cadastro e login concluidos

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
+import Login from './pages/Login';
+import Cadastro from './pages/Cadastro';
+import DadosP from './pages/DadosPessoais';
+import Endereco from './pages/Endereco';
+import Tipo from './pages/TipoConta';
+import Verificacao from './pages/CodeVerificacao';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+
+          <Route path="/" element={<Login />} />
+          <Route path="/Cadastro" element={<Cadastro />} />
+          <Route path="/Cadastro/2" element={<DadosP />} />
+          <Route path="/Cadastro/3" element={<Endereco />} />
+          <Route path="/Cadastro/4" element={<Tipo />} />
+          <Route path="/Cadastro/5" element={<Verificacao />} />
+
+        </Routes>
+      </Router>
     </div>
+
+
   );
 }
 
